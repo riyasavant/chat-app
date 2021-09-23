74a39d9c5870c0d2e1e9c52fb1603b913f421bef
San411 <sandeshbhat2000@gmail.com>
2021-09-23 16:54:37 +0530

    Login Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from "./Login";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
+    <Login/>
   </React.StrictMode>,
   document.getElementById('root')
 );
