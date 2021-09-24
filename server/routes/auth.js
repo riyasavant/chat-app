f002238586b000c7a74cda17d4e2e1214daeab00
Riya Savant <rieee2110@gmail.com>
2021-09-24 13:30:17 +0530

    Added Login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 const User = require('../models/user');
+
+router.post('/login', async (req, res) => {
+  const user = req.body;
+
+  User.findOne({ username: user.username })
+  .then(dbUser => {
+
+    // User not found case
+    if(!dbUser) {
+      res.status(401).json({ message: "Invalid Username or Password" })
+    } 
+    
+    // User with given username exists
+    else {
+      bcrypt.compare(user.password, dbUser.password)
+      .then(isMatching => {
+
+        // When the details are correct, we create a token and send it back
+        if(isMatching) {
+          const payload = {
+            id: dbUser._id,
+            username: dbUser.username
+          };
+
+          jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn: 86400},
+            (error, token) => {
+              if(error) return res.json({message: error})
+              return res.status(200).json({
+                message: "Logged in successfully",
+                token: "Bearer " + token
+              })
+            }
+          ) 
+        } 
+        
+        // The password does not match
+        else {
+          res.status(401).json({ message: "Invalid Password"});
+        }
+      })
+    }
+  })
+});
 
 router.post('/register', async (req, res) => {
   const user = req.body;
