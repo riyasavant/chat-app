194e314203dcefefd546cb654f96baf5f1b992c1
Riya Savant <rieee2110@gmail.com>
2021-09-25 22:24:50 +0530

    Fixed Toggle

--- client/src/components/Toggle/index.js
+++ client/src/components/Toggle/index.js
@@ -1,20 +1,18 @@
 
 import React from 'react';
-import { useContext } from 'react';
-import { ThemeContext } from '../../config/context/themeContext';
 import './index.css';
 
 export default function ToggleTheme({theme, setTheme}){
+
+    function changeTheme() {
+        theme === 'dark' ? setTheme('light') : setTheme('dark');
+    }
+
       return(
         <div id="toggle-pos">
             <label className="switch">
-            <input id="check" type="checkbox" />
-            <span className={theme === 'dark' ? 'slider-left':'slider'} onClick={() => {
-                if(theme === 'dark')
-                setTheme('light')
-                else
-                setTheme('dark')
-                }}></span>
+                <input id="check" type="checkbox" checked={theme === 'light' ? true : false}/>
+                <span className="slider" onClick={changeTheme}></span>
             </label>
         </div>
     )
