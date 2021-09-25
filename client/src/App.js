dbeeb49bb402c6493799a018847a8aa9cbf02c7e
Riya Savant <rieee2110@gmail.com>
2021-09-25 15:56:32 +0530

    Added ThemeContext for Dark / Light mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import SnackbarProvider from 'react-simple-snackbar'
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
+import { ThemeContext } from "./config/context/themeContext";
 
 function App() {
+
+  const [theme, setTheme] = useState('dark');
+
   return (
-    <SnackbarProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route component={Login} exact path="/" />
-          <Route component={Register} exact path="/register" />
-          <Route component={Chat} exact path="/chat"/>
-        </Switch>
-      </BrowserRouter>
-    </SnackbarProvider>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <SnackbarProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route component={Login} exact path="/" />
+            <Route component={Register} exact path="/register" />
+            <Route component={Chat} exact path="/chat"/>
+          </Switch>
+        </BrowserRouter>
+      </SnackbarProvider>
+    </ThemeContext.Provider>
   );
 }
 
