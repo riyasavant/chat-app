e0037548113b135aae3f36280e2397414193cd2f
San411 <sandeshbhat2000@gmail.com>
2021-11-08 19:13:00 +0530

    Increased contact height

--- client/src/components/UserChat/index.js
+++ client/src/components/UserChat/index.js
@@ -2,8 +2,8 @@ import "./index.css";
 
 export default function UserChat({ username }) {
     return(
-        <div className="flex mb-10 ctr">
-            <div className="profile-btn"></div>
+        <div className="flex mb-10 ctr" style={{height: '70px'}}>
+            <div className="profile-btn" style={{marginTop: '10px'}}></div>
             <div style={{marginLeft: '20px', display: 'flex', alignItems: 'center'}}>{username}</div>
         </div>
     )
